5dfc9d6509b3c7bb8554b4713efa55f6fea7ee94
Cozmin <cozmindeveloper@yahoo.com>
2022-06-01 15:28:12 +0000

    add api

--- src/api/Program.cs
+++ src/api/Program.cs
@@ -4,19 +4,18 @@ using Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
+
 Config config = new Config(){
-    ConnectionString = builder.Configuration.GetSection("Config").GetValue<string>("ConnectionString"),
-    DatabaseName = builder.Configuration.GetSection("Config").GetValue<string>("DatabaseName"),
+    ConnectionString = Environment.GetEnvironmentVariable("DBHOST"),
+    DatabaseName = Environment.GetEnvironmentVariable("DBNAME"),
 };
+
 builder.Services.AddSingleton<IConfig>(config);
 builder.Services.AddScoped<IMongoDBHelper, MongoDBHelper>();
 builder.Services.AddScoped<IJobRepository, JobRepository>();
 builder.Services.AddScoped<IJobService, JobService>();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
+
 
 var app = builder.Build();
 
